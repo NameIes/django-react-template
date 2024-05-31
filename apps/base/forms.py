45864a3e2ec57a976ed1338f7661519c290a6802
Nameless <nameies@yandex.ru>
2024-05-31 10:50:33 +0300

    Removed unused imports.

--- apps/base/forms.py
+++ apps/base/forms.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from dj_rest_auth.forms import AllAuthPasswordResetForm
 
 from .models import CustomUser
 
