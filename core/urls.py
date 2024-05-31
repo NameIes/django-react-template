3330c5ef6b3fbe21c6d5c3a117cc91a5f8a146a4
Nameless <nameies@yandex.ru>
2024-05-31 13:57:38 +0300

    Removed unused paths.

--- core/urls.py
+++ core/urls.py
@@ -45,8 +45,6 @@ if settings.DEBUG:
         path("__debug__/", include("debug_toolbar.urls")),
         path("admin/doc/", include("django.contrib.admindocs.urls")),
     ]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     # If you want to use Django Server Side Rendering then use this path.
