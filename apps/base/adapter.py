eaefb4a5fb420740dc534d008642a7346f1641d0
Nameless <nameies@yandex.ru>
2024-05-31 11:30:20 +0300

    Code formatted.

--- apps/base/adapter.py
+++ apps/base/adapter.py
@@ -1,13 +1,8 @@
 from django.conf import settings
-from urllib.parse import quote
+
 from allauth.account.adapter import DefaultAccountAdapter
 
 
 class ReactAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
-        return settings.CONFIRM_EMAIL_FRONTEND_URL + '?key=' + emailconfirmation.key
-
-    def get_reset_password_from_key_url(self, key):
-        parsed = quote(key)
-        print(parsed)
-        return settings.RESET_PASSWORD_FRONTEND_URL
+        return settings.CONFIRM_EMAIL_FRONTEND_URL + "?key=" + emailconfirmation.key
